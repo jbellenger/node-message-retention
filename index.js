25d63ed958a88d4086847a7cc0defb7ddba33651
jbellenger <jbellenger@twitter.com>
2016-11-17 14:01:45 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const heapdump = require('heapdump');
+
+const agent = new http.Agent({ keepAlive: true });
+const onServerReady = (server) => {
+  const opts = {
+    agent,
+    path: '/',
+    port: server.address().port,
+  };
+  http.request(opts, onRequest).end();
+};
+
+const onRequest = (res) => {
+  heapdump.writeSnapshot((err, filename) => {
+    console.log(`wrote ${require.resolve('./' + filename)}`);
+  });
+};
+
+const server = http
+  .createServer((req, res) => {
+    req.REQUEST_LOCAL_DATA = Buffer.alloc(30*1024*1024, 0);
+    res.end();
+  })
+  .listen(0, () => onServerReady(server));
+
